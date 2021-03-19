de2dacea4281cc41b11c0edf5c2691af3dd0691e
Rachik <jayhosee@gmail.com>
2021-03-19 05:58:21 +0300

    Create models: Courier, Order, Assign

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Courier(models.Model):
+    courier_type = models.CharField(max_length=20)
+    regions = models.CharField(max_length=50)
+    working_hours = models.CharField(max_length=100)
+    rating = models.FloatField(null=True)
+    earnings = models.IntegerField(null=True)
+
+
+class Order(models.Model):
+    weight = models.FloatField()
+    region = models.IntegerField()
+    delivery_hours = models.CharField(max_length=100)
+
+
+class Assign(models.Model):
+    courier = models.ForeignKey("Courier", on_delete=models.CASCADE)
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    assign_time = models.DateTimeField(auto_now_add=True)
+    completed_time = models.CharField(max_length=100, blank=True)
