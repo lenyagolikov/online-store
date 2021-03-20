7d3b8e349951b8542f6d4af0419cfbe17b123c06
Rachik <jayhosee@gmail.com>
2021-03-20 16:16:14 +0300

    Finish up POST request for couriers

--- delivery/views.py
+++ delivery/views.py
@@ -3,28 +3,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Courier, Order
 from .serializers import CourierSerializer
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework.serializers import ValidationError
+
+
+def validation_error(id_error_list):
+    pass
 
 
 @api_view(['POST'])
 def courier_list(request):
     data_list = request.data['data']
-    
+
     correct_data_list = []
     id_errors_list = []
 
     for data in data_list:
         serializer = CourierSerializer(data=data)
-
         if serializer.is_valid():
             correct_data_list.append(serializer)
         else:
             id_errors_list.append(serializer['courier_id'])
-        
+
     if id_errors_list == []:
         for data in correct_data_list:
             data.save()
-        correct_data_list.clear()
-        return Response(serializer.error_messages, status=status.HTTP_201_CREATED)            
-    
-    id_errors_list.clear()
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"couriers": [{"id": data['courier_id'].value} for data in correct_data_list]}, status=status.HTTP_201_CREATED)
+
+    return Response({"validation_error": {"couriers": [{"id": id.value} for id in id_errors_list]}}, status=status.HTTP_400_BAD_REQUEST)
